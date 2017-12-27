7e68303335dc0c94e0c9c305667c828a4ece0951
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-12-27 04:21:04 -0800

    Created User and Quote models

--- daily_quote/models.py
+++ daily_quote/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db.models import Model, CharField, TextField, ManyToManyField, ForeignKey
 
-# Create your models here.
+
+class User(Model):
+    name = CharField(max_length=30)
+    # password = CharField(max_length=50)
+    # salt = CharField(max_length=10)
+    quotes = ManyToManyField('Quote', through='Ranked')
+
+
+class Quote(Model):
+    author = CharField(max_length=45)
+    # profession = CharField(max_length=45)
+    text = TextField()
+
+
+class Ranked(Model):
+    user = ForeignKey(User)
+    quote = ForeignKey(Quote)
+    rank = 0
