18134eb5f6b433b499549a7cd513e42dfbadc5df
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-12-27 16:14:50 -0800

    set up an index route

--- daily_quote/views.py
+++ daily_quote/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .db_query import recommend_quote
+from .models import User, Quote, Ranked
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(req):
+    return HttpResponse()
