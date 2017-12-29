a45e1f14952d34e605777f714e3789ed077cb834
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-12-29 14:41:46 -0800

    Update daily quote query

--- daily_quote/db_query.py
+++ daily_quote/db_query.py
@@ -10,14 +10,15 @@ def random_row():
 
 def recommend_quote(user):
     try:
+        # Get all quotes the user likes
+        user_quotes = Quote.objects.filter(ranked__user=user, ranked__rank=1)
         # Pick a random quote the user likes
-        quote = choice(Quote.objects.filter(ranked__user=user, ranked__rank=1))
+        quote = choice(user_quotes)
         # Find other users who like that quote
         users = User.objects.filter(ranked__quote_id=quote.id, ranked__rank=1).exclude(id=user.id)
-        # Find other quotes those users like (excluding quotes already seen)
-        user_quotes = Quote.objects.filter(ranked__user=user)
+        # Find other quotes those users like (excluding quotes the user has already seen)
         quotes = Quote.objects.filter(user__in=users, ranked__rank=1).distinct()\
-                              .exclude(id=quote.id, user__quotes=user_quotes)
+                              .exclude(id=quote.id, user__quotes=Quote.objects.filter(ranked__user=user))
         # Return one at random
         return choice(quotes)
     except IndexError:
