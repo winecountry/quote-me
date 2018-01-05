c0cfc5ff0a47bede3a2f1bd0957f3461ce93a994
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-05 14:23:08 -0800

    Return Quote from QuoteRecommend. Add logic to handle state for disabling buttons.

--- daily_quote/models.py
+++ daily_quote/models.py
@@ -20,9 +20,39 @@ class Quote(models.Model):
 
     @staticmethod
     def recommend(profile=None):
-        quote = recommend_quote(profile)
+        if profile is None:
+            print("NO PROFILE SPECIFIED: Defaulting to random selection...")
+            return random_quote()
+
+        try:
+            # Get all quotes the user likes
+            profile_quotes = Quote.objects.filter(profile=profile)
+            profile_liked_quotes = profile_quotes.filter(quoterank__rank=1)
+
+            # Pick a random quote the user likes
+            quote = choice(profile_liked_quotes)
+
+            # Find other users who like that quote
+            profiles = Profile.objects.filter(quoterank__quote=quote, quoterank__rank=1).exclude(id=profile.id)
+
+            # Find other quotes those users like (excluding quotes the user has already seen)
+            quotes = Quote.objects.filter(profile__in=profiles, quoterank__rank=1).distinct()\
+                                  .exclude(id=quote.id, profile__quotes=profile_quotes)
+
+            # Return one at random
+            quote = choice(quotes)
+        except IndexError:
+            quote = random_quote()
+            print("NO SIMILAR QUOTES FOUND: Defaulting to random selection...", quote)
+
+        # Update last recommended quote
+        profile.current_quote_id = quote.id
+        profile.save()
+
+        print("CREATING QUOTERANK")
         quoterank, _ = QuoteRank.objects.get_or_create(profile=profile, quote=quote, rank=0)
-        return quoterank
+
+        return quoterank.quote
 
     def __str__(self):
         return '"{}" - {}'.format(self.text, self.author.name)
@@ -42,37 +72,3 @@ def random_quote():
     count = Quote.objects.all().count()
     pk = randint(1, count)
     return Quote.objects.get(pk=pk)
-
-
-def recommend_quote(profile):
-    if profile is None:
-        print("NO PROFILE SPECIFIED: Defaulting to random selection...")
-        return random_quote()
-
-    try:
-        # Get all quotes the user likes
-        profile_quotes = Quote.objects.filter(profile=profile)
-        profile_liked_quotes = profile_quotes.filter(quoterank__rank=1)
-
-        # Pick a random quote the user likes
-        quote = choice(profile_liked_quotes)
-
-        # Find other users who like that quote
-        profiles = Profile.objects.filter(quoterank__quote=quote, quoterank__rank=1).exclude(id=profile.id)
-
-        # Find other quotes those users like (excluding quotes the user has already seen)
-        quotes = Quote.objects.filter(profile__in=profiles, quoterank__rank=1).distinct()\
-                              .exclude(id=quote.id, profile__quotes=profile_quotes)
-
-        # Return one at random
-        quote = choice(quotes)
-    except IndexError:
-        quote = random_quote()
-        print("NO SIMILAR QUOTES FOUND: Defaulting to random selection...", quote)
-
-    # Update last recommended quote
-    profile.current_quote_id = quote.id
-    profile.save()
-
-    return quote
-
--- daily_quote/views.py
+++ daily_quote/views.py
@@ -6,27 +6,36 @@ from quote_me.models import Profile
 
 
 def user_home(request, username):
+    my_profile = False
+    rank = 0
+
     try:
         if username == 'me':
             profile = Profile.objects.get(user=request.user)
+            my_profile = True
         else:
             profile = Profile.objects.get(user__username=username)
+            if request.user.username == username:
+                my_profile = True
 
         try:
             quoterank = QuoteRank.objects.get(profile=profile, quote__id=profile.current_quote_id)
+            rank = quoterank.rank
 
             # return the current quote if it was recommended today, otherwise recommend a new quote
-            quote = quoterank.quote if quoterank.date == datetime.date.today() else Quote.recommend(profile).quote
+            quote = quoterank.quote if quoterank.date == datetime.date.today() else Quote.recommend(profile)
 
         except QuoteRank.DoesNotExist:
-            quote = Quote.recommend(profile).quote
+            quote = Quote.recommend(profile)
 
     except Profile.DoesNotExist:
-        quote = Quote.recommend().quote
+        quote = Quote.recommend()
 
     context = {
         'quote_text': quote.text,
-        'author': quote.author
+        'author': quote.author,
+        'my_profile': my_profile,
+        'rank': rank,
     }
 
     return render(request, 'daily_quote/index.html', context)
