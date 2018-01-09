e2d2129c765559523d5f6605fc2fc0b1973e6709
Nicholas Lofaro <5376991+nickoralias@users.noreply.github.com>
2018-01-08 17:59:38 -0800

    Add tests for quote_me

---
+++ quote_me/tests.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase
+
+class LoginTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('test', 'test@gmail.com', 'test')
+
+    def test_login(self):
+        self.client.login(username='test', password='test')
+        response = self.client.get('/')
+        user = User.objects.get(username='test')
+        self.assertEqual(response.context['user'], user)
+
+    def test_logout(self):
+        self.client.logout()
+        response = self.client.get('/')
+        self.assertEqual(response.context['user'], AnonymousUser())
