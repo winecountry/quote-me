7d8e467c87fe4c013c71c0dced25ad3f50c3b5dd
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-09 07:11:38 -0800

    Add test data and additional tests to daily_quote app

--- daily_quote/tests.py
+++ daily_quote/tests.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase, TransactionTestCase
+import json
+
+from django.contrib.auth.models import User
+from django.test import TransactionTestCase
 
 from daily_quote.models import Author, Quote
 from quote_me.models import Profile
@@ -9,26 +11,60 @@ class DailyQuoteTests(TransactionTestCase):
     reset_sequences = True
 
     def setUp(self):
-        author = Author.objects.create(name="John Smith", profession="Farmer")
-        Quote.objects.create(text="I like chicken", author=author)
-        User.objects.create_user('test', 'test@gmail.com', 'test')
+        create_quotes()
+        create_users()
 
-    def test_user_quote_rendered_to_profile_page(self):
-        self.client.login(username='test', password='test')
-        response = self.client.get('/test/')
+    def test_user_quote_rendered_to_home_page(self):
+        self.client.login(username='alice', password='pass1234')
+        response = self.client.get('/')
 
-        profile = Profile.objects.get(user__username='test')
+        profile = Profile.objects.get(user__username='alice')
         quote = response.context['quote']
 
         self.assertIsInstance(quote, Quote)
         self.assertEqual(quote.id, profile.current_quote_id)
 
-    def test_profile_page_has_like_controls(self):
-        self.client.login(username='test', password='test')
-        response = self.client.get('/test/')
+    def test_user_quote_rendered_to_profile_page(self):
+        self.client.login(username='alice', password='pass1234')
+        response = self.client.get('/alice/')
+
+        profile = Profile.objects.get(user__username='alice')
+        quote = response.context['quote']
+
+        self.assertIsInstance(quote, Quote)
+        self.assertEqual(quote.id, profile.current_quote_id)
+
+    def test_anonymous_user_quote_rendered_to_home(self):
+        response = self.client.get('/')
+        quote = response.context['quote']
+        self.assertIsInstance(quote, Quote)
+
+    def test_profile_page_has_rank_controls(self):
+        self.client.login(username='alice', password='pass1234')
+        response = self.client.get('/alice/')
         self.assertIs(response.context['my_profile'], True)
 
-    def test_user_page_has_no_like_controls(self):
-        self.client.login(username='test', password='test')
-        response = self.client.get('/other/')
+    def test_user_page_has_no_rank_controls(self):
+        self.client.login(username='alice', password='pass1234')
+        response = self.client.get('/bob/')
         self.assertIs(response.context['my_profile'], False)
+
+    def test_bad_response_for_nonexistent_username(self):
+        self.client.login(username='alice', password='pass1234')
+        response = self.client.get('/eve/')
+        self.assertEqual(response.status_code, 404)
+
+
+def create_quotes():
+    with open('daily_quote/test.json') as authors:
+        authors = json.load(authors)
+        for author in authors:
+            for quote in author['quotes']:
+                Quote.objects.create(author=Author.objects.create(name=author['name'],
+                                                                  profession=author['profession']),
+                                     text=quote['quote_string'])
+
+
+def create_users():
+    User.objects.create_user('alice', 'alice@gmail.com', 'pass1234')
+    User.objects.create_user('bob', 'bob@gmail.com', 'pass1234')
--- quote_me/tests.py
+++ quote_me/tests.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+
+from daily_quote.models import Author, Quote
 
 
-class LoginTests(TestCase):
+class LoginTests(TransactionTestCase):
+    reset_sequences = True
+
     def setUp(self):
-        user = User.objects.create_user('test', 'test@gmail.com', 'test')
+        author = Author.objects.create(name="John Smith", profession="Farmer")
+        Quote.objects.create(text="I like chicken", author=author)
+        User.objects.create_user('test', 'test@gmail.com', 'test')
 
     def test_login(self):
         self.client.login(username='test', password='test')
