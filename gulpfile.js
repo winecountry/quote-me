4c89624a53685e5ac0cfda9ddb05af0878ebbaf8
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-10 17:02:48 -0800

    Create gulp task for live reloading.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    browserSync = require('browser-sync').create(),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
     path = require('path');
 
+// define directories
 const STATIC_ROOT = 'static';
 const src = path.join(STATIC_ROOT, 'src');
 const dest = path.join(STATIC_ROOT, 'dist');
@@ -13,10 +15,17 @@ const css = path.join(dest, 'css');
 const js = path.join(dest, 'js');
 const sass = path.join(src, 'sass');
 
+// master sass files
 const sassSources = [
     path.join(sass, 'style.scss')
 ];
 
+// all js scripts
+const jsSources = [
+    path.join(src, 'scripts', '*.js')
+];
+
+// compile sass
 gulp.task('compass', function () {
     gulp.src(sassSources)
         .pipe(compass({
@@ -26,17 +35,34 @@ gulp.task('compass', function () {
             style: 'expanded'
         }))
         .pipe(gulp.dest(css))
+        .pipe(browserSync.stream());
 });
 
-const jsSources = [
-    path.join(src, 'scripts', '*.js')
-];
-
+// concatenate all js sources
 gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest(js))
+        .pipe(gulp.dest(js));
 });
 
-gulp.task('default', ['js', 'compass']);
+// ensure 'js' task is complete before browser reload
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('default', ['js'], function () {
+    browserSync.init({
+        proxy: "localhost:8000"
+    });
+
+    // reload page after html changes
+    gulp.watch(path.join('templates', '**/*.html')).on('change', browserSync.reload);
+
+    // inject css after sass changes
+    gulp.watch(path.join(sass, '*.scss'), ['compass']);
+
+    // reload after js changes
+    gulp.watch(path.join(js, '*.js'), ['js-watch']);
+});
