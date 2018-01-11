9a3127c70405c50be86c6427d535967b114963fc
Nicholas Lofaro <5376991+nickoralias@users.noreply.github.com>
2018-01-10 18:19:17 -0800

    Add tests using Selenium

--- quote_me/tests.py
+++ quote_me/tests.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase, TransactionTestCase
+from django.test import LiveServerTestCase, TestCase, TransactionTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from daily_quote.models import Author, Quote
 
 
-class LoginTests(TransactionTestCase):
+class LoginTestCase(TransactionTestCase):
     reset_sequences = True
 
     def setUp(self):
@@ -36,7 +38,7 @@ class LoginTests(TransactionTestCase):
         self.assertEqual(response.context['user'], AnonymousUser())
 
 
-class SignupTests(TransactionTestCase):
+class SignupTestCase(TransactionTestCase):
     reset_sequences = True
 
     def setUp(self):
@@ -59,3 +61,33 @@ class SignupTests(TransactionTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_active)
         self.assertEqual(response.context['user'], user)
+
+class FunctionalTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        self.selenium = webdriver.Safari()
+        super(FunctionalTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(FunctionalTestCase, self).tearDown()
+
+    def test_page_load(self):
+        selenium = self.selenium
+        selenium.get('http://localhost:8000')
+        
+        assert "No results found." not in selenium.page_source
+
+#    def test_register(self):
+#        selenium = self.selenium
+#        selenium.get('http://127.0.0.1:8000/signup/')
+#        first_name = selenium.find_element_by_id('id_first_name')
+#        last_name = selenium.find_element_by_id('id_last_name')
+#        username = selenium.find_element_by_id('id_username')
+#        email = selenium.find_element_by_id('id_email')
+#        password1 = selenium.find_element_by_id('id_password1')
+#        password2 = selenium.find_element_by_id('id_password2')
+#
+#        submit = selenium.find_element_by_name()
+
+        
