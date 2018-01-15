c025c96c04e2845a48a314eee14f4c1cd7ccd064
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-14 20:09:56 -0800

    Add buttons to home view.

--- daily_quote/views.py
+++ daily_quote/views.py
@@ -1,7 +1,22 @@
 from django.http import Http404
 from django.shortcuts import render
 
-from daily_quote.models import Profile, Quote
+from daily_quote.models import Profile, Quote, QuoteRank
+
+
+def home(request):
+    if request.user.is_authenticated:
+        quote = request.user.profile.recommend().quote
+    else:
+        quote = Quote.random_quote()
+
+    context = {
+        'current_route': 'home',
+        'quote': quote,
+        'rank': QuoteRank.objects.get(profile=request.user.profile, quote=quote).rank
+    }
+
+    return render(request, 'daily_quote/home.html', context)
 
 
 def user_profile(request, username):
@@ -27,17 +42,3 @@ def user_profile(request, username):
         raise Http404("Profile Does Not Exist")
 
     return render(request, 'daily_quote/profile.html', context)
-
-
-def home(request):
-    if request.user.is_authenticated:
-        quote = request.user.profile.recommend().quote
-    else:
-        quote = Quote.random_quote()
-
-    context = {
-        'current_route': 'home',
-        'quote': quote
-    }
-
-    return render(request, 'daily_quote/home.html', context)
