b096d536cb5bd90d0eaaf89afba38625b8a4dce0
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-16 02:23:51 -0800

    Render profile edit form to template

--- daily_quote/views.py
+++ daily_quote/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.forms import inlineformset_factory
 
-# from daily_quote.forms import ProfileEditForm, UserEditForm
+from daily_quote.forms import ProfileEditForm, UserEditForm
 from daily_quote.models import Profile, Quote, QuoteRank
 
 
@@ -30,24 +30,18 @@ def user_profile(request, username):
     }
 
     try:
+        user_edit_form = UserEditForm(instance=request.user)
+        profile_edit_form = ProfileEditForm(instance=request.user.profile)
+
         if request.user.username == username:
             context['my_profile'] = True
             profile = request.user.profile
 
-            # if request.method == 'POST':
-            #     user_edit_form = UserEditForm(request.POST, instance=request.user)
-            #     profile_edit_form = ProfileEditForm(request.POST, instance=request.user.profile)
-            #
-            #     if all(user_edit_form.is_valid(), profile_edit_form.is_valid()):
-            #         user = user_edit_form.save()
-            #         profile = profile_edit_form.save()
+            if all([user_edit_form.is_valid(), profile_edit_form.is_valid()]):
+                user_edit_form.save()
+                profile = profile_edit_form.save()
         else:
-            # user_edit_form = UserEditForm(instance=request.user)
-            # profile_edit_form = ProfileEditForm(instance=request.user.profile)
             profile = Profile.objects.get(user__username=username)
-
-        # context['user_edit_form'] = user_edit_form
-        # context['profile_edit_form'] = profile_edit_form
 
         quoterank = profile.recommend()
 
@@ -55,6 +49,8 @@ def user_profile(request, username):
         context['quote'] = quoterank.quote
         context['rank'] = quoterank.rank
         context['quotes'] = Quote.objects.filter(profile=profile)
+        context['user_edit_form'] = user_edit_form
+        context['profile_edit_form'] = profile_edit_form
 
     except Profile.DoesNotExist:
         raise Http404("Profile Does Not Exist")
