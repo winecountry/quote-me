c8979006128871df45cc6daa459c97e2596edeb7
Nicholas Lofaro <5376991+nickoralias@users.noreply.github.com>
2018-01-15 22:21:22 -0800

    Update db scripts

---
+++ test_db_init.sql
@@ -0,0 +1,9 @@
+DROP ROLE IF EXISTS quote_admin;
+CREATE ROLE quote_admin LOGIN PASSWORD 'password';
+
+ALTER ROLE quote_admin SET client_encoding TO 'utf8';
+ALTER ROLE quote_admin SET default_transaction_isolation TO 'read committed';
+ALTER ROLE quote_admin SET timezone TO 'UTC';
+
+GRANT ALL PRIVILEGES ON DATABASE test_db TO quote_admin;
+
