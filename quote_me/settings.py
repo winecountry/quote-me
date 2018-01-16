e82c31e4bff43fe71542aa4056957db8f739c4ce
Nicholas Lofaro <5376991+nickoralias@users.noreply.github.com>
2018-01-16 11:08:30 -0800

    Fix typo

--- quote_me/settings.py
+++ quote_me/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = 'static/dist/'
- STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
